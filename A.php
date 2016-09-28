fb634d42757c62bd983ba3dcb921cb8c0d53c69d
eomgisu <eomgisu94@gmail.com>
2016-09-28 19:47:09 -0400

    Initial test

---
+++ A.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>A</title>
+</head>
+<body>
+
+<?php include 'Menu.php';?>
+
+<a href="A_menu.php">Menu</a>
+
+<a href="A_order.php">Order</a>
+
+</body>
+</html>
